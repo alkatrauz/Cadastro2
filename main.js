475f61917bc07c4f25fca9801127c756a873c562
alkatrauz <163009399+alkatrauz@users.noreply.github.com>
2024-10-17 19:34:11 -0300

    alteracao

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+
 let formulario = document.getElementById('form-cadastro');
 let tbody = document.querySelector('#tabela-cadastros tbody');
 formulario.querySelector('button').onclick = function() {
-  let linha = document.createElement('tr');
-  linha.innerHTML = `<td>${formulario.nome.value}</td><td>${formulario.email.value}</td>`;
-  tbody.innerHTML += linha.outerHTML;
-  formulario.reset();
+  // Obtém o valor dos campos de nome e email
+  let nome = formulario.nome.value;
+  let email = formulario.email.value;
+
+  // Cria uma nova linha de HTML para a tabela
+  tbody.innerHTML += `<tr><td>${nome}</td><td>${email}</td></tr>`;
+
+  // Limpa os campos do formulário
+  formulario.reset(); // Limpa o formulário
 };
